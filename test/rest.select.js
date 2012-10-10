6f96c3495bd7f7271d09617dd6b622a039acba4d
Aravind <aravindet@gmail.com>
2012-10-10 19:00:26 +0530

    Added REST handler, fixes for insert and update

--- test/rest.select.js
+++ test/rest.select.js
@@ -1,71 +1,42 @@
-resql = require("../lib/resql");
+var rs = require("../lib/resql"),
+	http = require("http"),
+    db = new rs.connect({
+		dialect: "mysql",
+		host: "localhost",
+		user: "askabt",
+		password: "askabt",
+		database: "askabt"
+	});
 
-var model = new resql();
-
-model.table('users', {
-	"id": "asdf",
-	"nick": "asdf",
-	
-	"subscriptions": new Resty.Children("subscriptions", "userId")
+var students = db.table('students', {
+	name: rs.String,
+	dob:  rs.Date,
 });
 
-model.table('subscriptions', {
-	"id": "asdfasd",
-	"user": new Resty.Parent("users", "user")
+var courses = db.table('courses', {
+	id:        rs.String,
+	name:      rs.String,
+	desc:      rs.Text,
+	teacherId: rs.Foreign('teachers'),
+	
+	faculty:   rs.Relation('teacherId')
 });
 
-console.log(model.get('subscriptions/1234/user/subscriptions'));
-
-
-model.users("asdfsd").subscriptions().then(function(subscriptions) {
+var teachers = db.table('teachers', {
+	name:   rs.String,
+	joined: rs.Date,
 	
+	courses: rs.Relation('courses', 'teacherId')
 });
 
-/*	
- *
- *	Javascript API:
- *
- *	model = new resql(db_options);
- *
- *	model.define()
+// db.sync(true);
 
-	example API:
-	
-		wh/users/100
-		
-		SELECT * FROM users WHERE id = 100
-	
-		wh/users/100/addresses
-		
-		SELECT * FROM addresses WHERE userId = (SELECT id FROM users WHERE id = 100)
-		
-		wh/addresses/<address>/user
-		
-		SELECT * FROM users WHERE id = (SELECT userId FROM addresses WHERE id = 100)
-	
-		wh/addresses/<address>/user/addresses
-		
-		SELECT * FROM addresses WHERE userId = (SELECT id FROM users WHERE id = (SELECT userId FROM addresses WHERE id = <address>))
-		SELECT * FROM addresses WHERE userId = (SELECT userId FROM addresses WHERE id=<address>)
-	
-		wh/addresses/<address>/user/addresses/add
-		
-		INSERT INTO addresses SET userId = (SELECT userId FROM addresses WHERE id=<address>)
-		
-		wh/topics/<topic>/conversations/add
-		
-		INSERT INTO conversations SET topicId = <topic>
- *
- *	resql.table(name, schema)
- *
-	:tablename			table	q.tablename = :tablename
-	[table]/:id			row		q.filters.id = :id
-	[row]/:foreign		row		ctx.id=subquery:SELECT foreign.colname,
-								ctx.tablename=foreign.tablename
-	[object]/:remote	table
-	
-	GET array			SELECT opt.fields FROM ctx.tablename WHERE opt.filters
-	GET object			SELECT opt.fields FROM ctx.tablename WHERE id=ctx.id
-
- *
- */
+var handler = db.makeRest({});
+var form = require("fs").readFileSync("index.html");
+http.createServer(function(req, res) {
+	if(req.url == "/")
+		res.end(form);
+	else
+		handler(req, res);
+}).listen(9999);
+console.log("http://localhost:9999");
